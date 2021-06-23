7baf49508e655d24b295ec4b56d9b50dc381cf69
vimarteleto <viniciusmarteleto@gmail.com>
2021-06-23 17:51:13 -0300

    Recursos e regras do Firebase, formulários React, recursos de criação de perguntas

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,23 @@
+import copyImg from '../assets/images/copy.svg'
+
+import '../styles/room-code.scss'
+
+type RoomCodeProps = {
+    code: string
+}
+
+export function RoomCode(props: RoomCodeProps) {
+
+    function copyRoomCodetoClipboard() {
+        return navigator.clipboard.writeText(props.code)
+    }
+
+    return (
+        <button className="room-code" onClick={copyRoomCodetoClipboard}>
+            <div>
+                <img src={copyImg} alt="Copy room code"/>
+            </div>
+            <span>Sala {props.code}</span>
+        </button>
+    )
+}
